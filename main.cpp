2fd27b775acb01edff5832ada1404d31922b4ff4
LDK <gritdrake@gmail.com>
2024-05-28 12:59:57 +0900

    반복추가

--- main.cpp
+++ main.cpp
@@ -14,15 +14,29 @@ int main()
 	Persons[1] = new FPlayer();
 	FDeck MyDeck;
 	MyDeck.Suffle();
-
-	cout << "Card 11 : J , 12 : Q , 13 : K  // 10으로 계산" <<endl<< endl;
-
-	for (int Index = 0; Index < 2; ++Index)
+	int IsRunning = true;
+	while (IsRunning)
 	{
-		Persons[Index]->GetCard(MyDeck);
-	}
-	dynamic_cast<FDealer*>(Persons[0])->DeclareVictory(dynamic_cast<FPlayer*>(Persons[1])->PlayerCard[0], dynamic_cast<FPlayer*>(Persons[1])->PlayerCard[1]);
+		int StartGame = 0;
+		cin >> StartGame;
 
+		if (StartGame == 1)
+		{
+			cout << "Card 11 : J , 12 : Q , 13 : K  // 10으로 계산" << endl << endl;
+
+			for (int Index = 0; Index < 2; ++Index)
+			{
+				Persons[Index]->GetCard(MyDeck);
+			}
+			dynamic_cast<FDealer*>(Persons[0])->DeclareVictory(dynamic_cast<FPlayer*>(Persons[1])->PlayerCard[0], dynamic_cast<FPlayer*>(Persons[1])->PlayerCard[1]);
+			cout << "계속하려면 1 , 끝내려면 0 입력" << endl<<endl;
+		}
+		if(StartGame == 0)
+		{
+			IsRunning = false;
+		}
+	}
+	
 	for (int Index = 0; Index < 2; ++Index)
 	{
 		delete Persons[Index];
